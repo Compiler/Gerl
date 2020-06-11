8c581d53477292378d19f64a2235f820f2fb4cf6
Luke <lr98lr@aol.com>
2020-06-11 12:05:03 -0400

    added and linked all libs and fixed makefile

--- makefile
+++ makefile
@@ -1,12 +1,29 @@
 CXX = gcc
+CXXFLAGS = -Wall
 
-INC=-I src/ -I outsourced/glad/include/ -I outsourced/glfw-3.3.2/MinGWmingw32/include/
-LIBS = -L outsourced/glfw-3.3.2/MinGWmingw32/lib/
+OUT_DIR = bin
+LAUNCHER_NAME = Gerl
+SRC_DIR = src
+ENTRY_POINT = $(SRC_DIR)/main.c
+
+GLAD_ROOT = outsourced/glad/
+GLAD_INC = $(GLAD_ROOT)include
+GLAD_SRC = $(GLAD_ROOT)src
+
+GLFW_ROOT = outsourced/glfw-3.3.2/MinGWmingw32/
+GLFW_INC = $(GLFW_ROOT)include
+GLFW_LIB = $(GLFW_ROOT)lib
+GLFW_SRC = $(GLFW_ROOT)../src
+
+
+INC=-I src/ -I $(GLAD_INC) -I $(GLFW_INC)
+LIBS = -L $(GLFW_LIB)
+LINKS = -lglfw3 -lglu32 -lopengl32 -lgdi32
 
 all: main
 
-main: src/main.c
-	$(CXX) $(LIBS) $(INC) -o bin/Gerl src/main.c -lglfw3 -lopengl32 -lglu32 -lgdi32
+main: $(ENTRY_POINT)
+	$(CXX) $(CXXFLAGS) $(LIBS) $(INC) -o $(OUT_DIR)/$(LAUNCHER_NAME) $(ENTRY_POINT) $(GLAD_SRC)/glad.c $(LINKS)
 
-run: bin/Gerl.exe
-	./bin/Gerl.exe
+run: $(OUT_DIR)/$(LAUNCHER_NAME).exe
+	./$(OUT_DIR)/$(LAUNCHER_NAME).exe
--- src/main.c
+++ src/main.c
@@ -1,22 +1,32 @@
-#include <stdio.h>
-#include "Core/GerlCore.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-//#include <glad/gl.h>
+#include <stdio.h>
+#include <Core/GerlCore.h>
 
 int main(){
 	if(!glfwInit()) printf("Failed to init glfw");
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Something went wrong!\n");
+	}
+	if(!gladLoadGL()) {
+        printf("Something went wrong!\n");
+	}
    	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);//version 4.6 Core profile
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "C OPENGL", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	
+
 	struct GerlCore core;
 	core.number = 4;
 	printf("Hello %d", core.number);
 
-	float time = glfwGetTime();
-	while(glfwGetTime() - time <= 5) {
+	while(glfwWindowShouldClose(window)) {
+		glClearColor(0.2, 0.1, 0.2, 1.0);
 		glfwPollEvents();
 	}
 	
