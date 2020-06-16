b9af0af4ff563ff0c436615dcd6d50f05268a798
Luke <lr98lr@aol.com>
2020-06-15 20:02:28 -0400

    some more math

--- src/Math/Vec3.c
+++ src/Math/Vec3.c
@@ -1,25 +1,53 @@
 #include "Vec3.h"
 
+
+float length(vec3* vecA){
+    return sqrt(vecA->x * vecA->x + vecA->y  * vecA->y + vecA->z * vecA->z );
+}
+
 void addTo(vec3 *vecA, vec3* vecB){
     vecA->x += vecB->x;
     vecA->y += vecB->y;
     vecA->z += vecB->z;
+}
+void addTo(vec3 *vecA, float value){
+    vecA->x += value;
+    vecA->y += value;
+    vecA->z += value;
 }
 void subFrom(vec3 *vecA, vec3* vecB){
     vecA->x -= vecB->x;
     vecA->y -= vecB->y;
     vecA->z -= vecB->z;
 }
+void subFrom(vec3 *vecA, float value){
+    vecA->x -= value;
+    vecA->y -= value;
+    vecA->z -= value;
+}
 void mulTo(vec3 *vecA, vec3* vecB){
     vecA->x *= vecB->x;
     vecA->y *= vecB->y;
     vecA->z *= vecB->z;
+}
+void mulTo(vec3 *vecA, float value){
+    vecA->x *= value;
+    vecA->y *= value;
+    vecA->z *= value;
 }
 void divTo(vec3 *vecA, vec3* vecB){
     vecA->x /= vecB->x;
     vecA->y /= vecB->y;
     vecA->z /= vecB->z;
 }
-//void subFrom(vec3* vecA, vec3* vecB);
-//void mulTo(vec3* vecA, vec3* vecB);
-//void divTo(vec3* vecA, vec3* vecB);
+void divTo(vec3 *vecA, float value){
+    vecA->x /= value;
+    vecA->y /= value;
+    vecA->z /= value;
+}
+
+void unitVector(vec3* vecA){
+    vec3 ref;
+    ref.x = vecA->x;ref.y = vecA->y;ref.z = vecA->z;
+
+}
--- src/Math/Vec3.h
+++ src/Math/Vec3.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <math.h>
 
 typedef struct vec3{
     float x;
@@ -6,7 +7,15 @@ typedef struct vec3{
     float z;
 }vec3;
 
+
+float length(vec3* vecA);
 void addTo(vec3* vecA, vec3* vecB);
+void addTo(vec3* vecA, float val);
 void subFrom(vec3* vecA, vec3* vecB);
+void subFrom(vec3* vecA, float val);
 void mulTo(vec3* vecA, vec3* vecB);
+void mulTo(vec3* vecA, float val);
 void divTo(vec3* vecA, vec3* vecB);
+void divTo(vec3* vecA, float val);
+
+void unitVector(vec3* vecA);
